37194f97dc90ff2111714a518f36ec17ec429b65
latrokles <latrokles@gmail.com>
2023-02-25 20:46:07 -0800

    feat: test sdl usage as an abstraction layer to the os.
    wrote a simple sdl program that paints a pixel when the mouse is
    clicked. first time I play with the scaling of the resolution in sdl.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # yakdraw
-some 2D graphics programming with python and sdl2... 
+
+This repository tracks some personal experiments with 2D graphics, mainly implementing some basic 2D graphics primitives and a simple software based 2D raster graphics library.
+
+The main idea is to simulate a framebuffer as an array of pixel values and draw to it using software implementations of 2D graphics algorithms (all CPU, NO graphics hardware).
+
+Since - at the end of the day - I want to display these experiments I have to take a dependency on something that can put things on a OS window (and, thus, the screen). I've chosen SDL2 for its portability, but this could be anything that can take an array of pixels (integer values) and write display them on the screen (a NSWindow + NSImage in macos, a X11 window in linux, not sure about Windows tho).
+
+I'm also using python as the implementation language in the interest of expediency despite the performance penalty it incurs with this type of software. I've been doing a lot of python programming the past couple of years at work so it's really fresh in my head and I know at least one other person who may actually read and play with this code if it's in python. There are a couple other languages I'd like to implement this on, but I'd be learning too many things... I can always re-implemented in other languages later.
+
+
+## inspiration and references
+
+I'm drawing from stuff that is quite old and perhaps out of date because - quite frankly - more recent resources on graphics bring with them the advances (and let's be honest, the complexity) of modern graphics hardware.
+
+- Plan9's libdraw
+- Smalltalk 80's BitBlt
+- Fundamentals of Interactive Computer Graphics, J.D. Foley, A. Van Dam,  
+- Graphics Gems, Andrew S. Galssner (editor)
---
+++ yakdraw/cmd/sdl.py
@@ -0,0 +1,78 @@
+import ctypes
+import sdl2
+import sys
+
+WIN_WIDTH = 640
+WIN_HEIGHT = 480
+
+FB_WIDTH = 320
+FB_HEIGHT = 240
+
+def test():
+    # ignoring all sdl errors here
+
+    sdl2.SDL_Init(sdl2.SDL_INIT_VIDEO)
+
+    window = sdl2.SDL_CreateWindow('sdltest'.encode('utf-8'),
+                                   sdl2.SDL_WINDOWPOS_UNDEFINED,
+                                   sdl2.SDL_WINDOWPOS_UNDEFINED,
+                                   WIN_WIDTH,
+                                   WIN_HEIGHT,
+                                   0)
+    
+    renderer = sdl2.render.SDL_CreateRenderer(window,
+                                              -1,
+                                              sdl2.SDL_RENDERER_PRESENTVSYNC)
+
+    sdl2.SDL_SetWindowMinimumSize(window, FB_WIDTH, FB_HEIGHT)
+
+    sdl2.render.SDL_RenderSetLogicalSize(renderer, FB_WIDTH, FB_HEIGHT)
+    sdl2.render.SDL_RenderSetIntegerScale(renderer, 1)
+
+    texture = sdl2.SDL_CreateTexture(renderer,
+                                     sdl2.SDL_PIXELFORMAT_RGBA8888,
+                                     sdl2.SDL_TEXTUREACCESS_STREAMING,
+                                     FB_WIDTH,
+                                     FB_HEIGHT)
+
+    # fb = bytearray([0xFF]*FB_WIDTH*FB_HEIGHT*4)
+    fb = bytearray(FB_WIDTH * FB_HEIGHT * 4)
+    for y in range(FB_HEIGHT):
+        for x in range(FB_WIDTH):    
+            pix = (x * 4) + (y * FB_WIDTH * 4)
+            fb[pix] = 0xff
+            fb[pix + 1] = 0xff
+            fb[pix + 2] = 0xff
+            fb[pix + 3] = 0xff
+
+    while True:        
+
+        event = sdl2.SDL_Event()
+        while sdl2.SDL_PollEvent(event) != 0:
+            if event.type == sdl2.SDL_QUIT:
+                sys.exit(0)
+
+            if event.type == sdl2.SDL_MOUSEBUTTONDOWN:
+                x = event.button.x
+                y = event.button.y
+
+                # get pixel position in framebuffer
+                pix = (x * 4) + (y * FB_WIDTH * 4)
+
+                # make it blue
+                fb[pix] = 0xff
+                fb[pix + 1] = 0xff
+                fb[pix + 2] = 0x00
+                fb[pix + 3] = 0x00
+         
+        # copy framebuffer to texture and render it in sdl window
+        # 1. get a pointer to the framebuffer memory (can't avoid C eh?)
+        fb_pointer = (ctypes.c_char * len(fb)).from_buffer(fb)
+
+        # 2. copy framebuffer into texture
+        sdl2.SDL_UpdateTexture(texture, None, fb_pointer, FB_WIDTH * 4)
+
+        # 3. display the texture in sdl window
+        sdl2.SDL_RenderClear(renderer)
+        sdl2.SDL_RenderCopy(renderer, texture, None, None)
+        sdl2.SDL_RenderPresent(renderer)
