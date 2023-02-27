c4ee7d4c52455702acb378d30f7706da8f161b80
latrokles <latrokles@gmail.com>
2023-02-26 21:46:12 -0800

    fix: correct loop limits in example 4

--- yakdraw/examples/_04_lines2.py
+++ yakdraw/examples/_04_lines2.py
@@ -18,11 +18,41 @@ SCALE = 2
 
 @dataclass
 class Lines2(Sketch):
+    margin: int = 20
 
     def setup(self):
         for y in range(self.height):
             for x in range(self.width):
                 self.canvas.put_pixel(x, y, Palette.WHITE)
+
+        # this could be a rect!
+        draw_line(self.canvas,
+                  self.margin,
+                  self.margin,
+                  self.width - self.margin,
+                  self.margin,
+                  Palette.RED1)
+
+        draw_line(self.canvas,
+                  self.width - self.margin,
+                  self.margin,
+                  self.width - self.margin,
+                  self.height - self.margin,
+                  Palette.RED1)
+
+        draw_line(self.canvas,
+                  self.width - self.margin,
+                  self.height - self.margin,
+                  self.margin,
+                  self.height - self.margin,
+                  Palette.RED1)
+
+        draw_line(self.canvas,
+                  self.margin,
+                  self.height - self.margin,
+                  self.margin,
+                  self.margin,
+                  Palette.RED1)
 
         startx, starty = 20, 20
         endx, endy = self.width - 20, self.height - 20
@@ -30,12 +60,12 @@ class Lines2(Sketch):
         x0, y0 = startx, starty
         x1, y1 = startx, endy
 
-        while (y0 <= endy) and (x1 <= endx):
+        while (y0 < endy) and (x1 < endx):
             draw_line(self.canvas, x0, y0, x1, y1, Palette.random())
             y0 += 10
             x1 += 10
 
-        while (x0 <= endx) and (y1 >= starty):
+        while (x0 < endx) and (y1 > starty):
             draw_line(self.canvas, x0, y0, x1, y1, Palette.random())
             x0 += 10
             y1 -= 10
