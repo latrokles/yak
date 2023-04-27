ac2d78cf8bb1b9304138e0496b044b9cef39d892
latrokles <latrokles@gmail.com>
2023-04-26 19:13:55 -0700

    feat: add simplistic vm execution loop

--- yak/cmd/vm.py
+++ yak/cmd/vm.py
@@ -1,12 +1,16 @@
 from yak.vm.debug import disassemble_chunk
 from yak.vm.chunk import Chunk
 from yak.vm.opcode import Opcode
+from yak.vm.vm import VirtualMachine
 
 
 def yak():
+    vm = VirtualMachine()
+
     chunk = Chunk()
     value = chunk.add_constant(1.2)
     chunk.write(Opcode.OP_CONSTANT, 123)
     chunk.write(value, 123)
     chunk.write(Opcode.OP_RETURN, 123)
     disassemble_chunk(chunk, 'test chunk')
+    vm.interpret(chunk)
--- yak/vm/vm.py
+++ yak/vm/vm.py
@@ -1,5 +1,10 @@
 from dataclasses import dataclass, field
+from enum import Enum
 from typing import Any
+
+from yak.vm.chunk import Chunk
+from yak.vm.opcode import Opcode
+from yak.vm.value import Value, print_value
 
 
 class VMError(Exception):
@@ -16,8 +21,18 @@ class CallFrame:
     pass
 
 
+class InterpretResult(Enum):
+    INTERPRET_OK = 0x00
+    INTERPRET_COMPILE_ERROR = 0x01
+    INTERPRET_RUNTIME_ERROR = 0x10
+
+
+
 @dataclass
 class VirtualMachine:
+    chunk: Chunk|None = None
+    ip: int = 0
+
     d_stack: list[Any] = field(default_factory=list)
     c_stack: list[CallFrame] = field(default_factory=list)
 
@@ -32,3 +47,28 @@ class VirtualMachine:
 
     def push_value(self, value: Any):
         self.d_stack.append(value)
+
+    def interpret(self, chunk: Chunk) -> InterpretResult:
+        self.chunk = chunk
+        return self.run()
+
+    def run(self) -> InterpretResult:
+        while True:
+            match (instruction := self.read_byte()):
+                case Opcode.OP_CONSTANT:
+                    constant = self.read_constant()
+                    print_value(constant)
+                    print()
+                    break;
+                case Opcode.OP_RETURN:
+                    return InterpretResult.INTERPRET_OK
+                case _:
+                    return InterpretResult.INTERPRET_RUNTIME_ERROR
+
+    def read_byte(self) -> int:
+        instruction = self.chunk.code[self.ip]
+        self.ip += 1
+        return instruction
+
+    def read_constant(self) -> Value:
+        return self.chunk.constants[self.read_byte()]
