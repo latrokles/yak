e498ffc9e0f3dba06b009671adf423e448c3382e
latrokles <latrokles@gmail.com>
2023-06-22 08:06:03 -0700

    play: exploring some of smalltalk's graphics primitives

--- yak/draw/bitblt.py
+++ yak/draw/bitblt.py
@@ -1,12 +1,43 @@
 from dataclasses import dataclass
-from enum import Enum
+from enum import IntEnum, auto
 
 from .color import Color
 from .geometry import Rectangle
 
+"""
+Exploring some of smalltalk's graphics primitives here
+"""
+
+@dataclass
+class Bitmap:
+    memory_bytes: bytearray
+
+
+@dataclass
+class Form:
+    width: int
+    height: int
+    depth: int
+    bitmap: Bitmap
+
 
 class CombinationRule(Enum):
-    pass
+    ALL_ZEROS                            = 0
+    SOURCE_AND_DESTINATION               = 1
+    SOURCE_AND_DESTINATION_INVERT        = 2
+    SOURCE_ONLY                          = 3
+    SOURCE_INVERT_AND_DESTINATION        = 4
+    DESTINATION_ONLY                     = 5
+    SOURCE_XOR_DESTINATION               = 6
+    SOURCE_OR_DESTINATION                = 7
+    SOURCE_INVERT_AND_DESTINATION_INVERT = 8
+    SOURCE_INVERT_XOR_DESTINATION        = 9
+    DESTINATION_INVERT                   = 10
+    SOURCE_OR_DESTINATION_INVERT         = 11
+    SOURCE_INVERT                        = 12
+    SOURCE_INVERT_OR_DESTINATION         = 13
+    SOURCE_INVERT OR DESTINATION_INVERT  = 14
+    ALL_ONES                             = 15
 
 
 @dataclass
